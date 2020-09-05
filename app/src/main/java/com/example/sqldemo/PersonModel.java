903937cc9aa62be81affb0183b9abc08d02b72ca
krillebjoerk@live.se <krillebjoerk@live.se>
2020-09-05 11:17:03 +0200

    Gui and added person class

--- app/src/main/java/com/example/sqldemo/PersonModel.java
+++ app/src/main/java/com/example/sqldemo/PersonModel.java
@@ -2,8 +2,62 @@ package com.example.sqldemo;
 
 public class PersonModel {
 
+    public PersonModel(String firstName, int age, boolean isActive)
+    {
+        this.firstName = firstName;
+        this.age = age;
+        this.isActive = isActive;
+    }
+
+    public PersonModel()
+    {
+
+    }
+
+    @Override
+    public String toString() {
+        return "PersonModel{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", age=" + age +
+                ", isActive=" + isActive +
+                '}';
+    }
+
     private int id;
     private String firstName;
     private int age;
     private boolean isActive;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
 }
